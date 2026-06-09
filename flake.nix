7a8f12cde61e3e20d40bffa106c6bdc03f913283
boyreau <bnzlvosnb@mozmail.com>
2026-06-09 16:02:50 +0200

    pinned nixos/nixpkgs-26.05 for reproductibility

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Alwide - A modern terminal-based code editor";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/26.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
